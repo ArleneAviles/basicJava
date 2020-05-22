8999baf85b3a313129dcf06b173edca36503cefd
Aviles Alvarado <kjtk951@astrazeneca.net>
2020-05-21 19:59:09 -0500

    Se agrega la tarea de los Elefantes

---
+++ src/iLoops/CancionElefantes.java
@@ -0,0 +1,22 @@
+package iLoops;
+
+public class CancionElefantes {
+
+    public static void main(String[] args) {
+        String plural = " Elefantes se columpiaban";
+
+        for (int i=1;i<=100;i++){
+            if (i == 1){
+                System.out.println(i + " Elefante se columpiaba sobre la tela de una araña,\n" +
+                        "como veia que resistia, fue a llamar a otro elefante");
+            }
+            else{
+                System.out.println(i + plural + " sobre la tela de una araña,\n" +
+                        "como veian que resistian, fueron a llamar a otro elefante");
+            }
+
+        }
+
+    }
+
+}
